14be8da66caf39fa9fcfc81965e9142fe4f7e5d0
Bogdyans <BOGDYA2004@gmail.com>
2024-11-09 15:05:39 +0300

    feat(app): create repo and add main routes pages

---
+++ src/app/admin_page/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
+export default function AdminPage() {
+    return (
+        <div className="max-w-2xl mx-auto space-y-8">
+            <h1 className="text-4xl font-bold tracking-tight text-center">Hello, Admin</h1>
+            <div className="flex justify-center gap-4">
+                <Link href="/account">
+                    <Button variant="outline">Go to Personal Account</Button>
+                </Link>
+                <Button className={" bg-black white"}>Logout</Button>
+            </div>
+        </div>
+    )
+}
