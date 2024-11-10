5387db3d76a79c004ef99c1d02d6bea1f950889f
Bogdyans <BOGDYA2004@gmail.com>
2024-11-10 15:37:22 +0300

    feat(app): add working admin-page, clean unnecessary stuff

--- src/app/admin-page/page.tsx
+++ src/app/admin-page/page.tsx
@@ -1,7 +1,16 @@
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
+import {authOptions} from "@/app/api/auth/[...nextauth]/authOptions";
+import {getServerSession} from "next-auth";
+import {redirect} from "next/navigation";
 
-export default function AdminPage() {
+export default async function AdminPage() {
+    const session = await getServerSession(authOptions)
+
+    if (!session || !session.user.groups?.includes("delete-account")) {
+        redirect('/account');
+    }
+
     return (
         <div className="max-w-2xl mx-auto space-y-8">
             <h1 className="text-4xl font-bold tracking-tight text-center">Hello, Admin</h1>
--- src/app/api/auth/[...nextauth]/authOptions.ts
+++ src/app/api/auth/[...nextauth]/authOptions.ts
@@ -1,12 +1,39 @@
 import { AuthOptions } from "next-auth";
 import KeycloakProvider from "next-auth/providers/keycloak";
 
+// export const authOptions: AuthOptions = {
+//     providers: [
+//         KeycloakProvider({
+//             clientId: process.env.KEYCLOAK_CLIENT_ID!,
+//             clientSecret: process.env.KEYCLOAK_CLIENT_SECRET!,
+//             issuer: process.env.KEYCLOAK_ISSUER!
+//         })
+//     ]
+// };
 export const authOptions: AuthOptions = {
     providers: [
         KeycloakProvider({
             clientId: process.env.KEYCLOAK_CLIENT_ID!,
             clientSecret: process.env.KEYCLOAK_CLIENT_SECRET!,
-            issuer: process.env.KEYCLOAK_ISSUER!
-        })
-    ]
-};
+            issuer: process.env.KEYCLOAK_ISSUER,
+        }),
+    ],
+    callbacks: {
+        async jwt({ token, account }) {
+            if (account) {
+                token.accessToken = account.access_token
+                // Assuming the groups are returned in the access token's payload
+                const payload = JSON.parse(Buffer.from(account.access_token!.split('.')[1], 'base64').toString())
+
+                token.groups = payload.resource_access.account.roles || []
+            }
+            return token
+        },
+        async session({ session, token }) {
+            session.accessToken = token.accessToken
+            session.user.groups = token.groups
+            //console.log(session)
+            return session
+        },
+    },
+}
